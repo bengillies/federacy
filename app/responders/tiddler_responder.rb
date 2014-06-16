6c4381a6f27553ec1200828d7fba3e13ee156541
Ben Gillies <bengillies@gmail.com>
2014-06-16 15:33:00 +0100

    Allow non binary tiddlers to be sent with their content type
    This follows the same rules as the binary tiddler handling support added
    in 221f38b, but if the tiddler is text based, uses send_data instead of
    send_file.
    
    This also adds another rule such that if the tiddler is markdown, it
    always defaults to html rendering.

--- app/responders/tiddler_responder.rb
+++ app/responders/tiddler_responder.rb
@@ -1,20 +1,39 @@
 class TiddlerResponder < ActionController::Responder
   def respond
-    if should_send_binary?
-      @format = 'binary'
+    if should_send_raw?
+      @format = raw_type
     end
 
     super
   end
 
-  def should_send_binary?
+  # Send the resource body directly with it's decared content type only if there
+  # is no explicitly declared format/accept header, and it's a thing that has a
+  # content type, and it's content type doesn't render into HTML (i.e. some sort
+  # of wikitext, currently only markdown).
+  def should_send_raw?
     request.headers["HTTP_ACCEPT"].include?("*/*") &&
       controller.params[:format].nil? &&
-      resource.respond_to?(:binary?) && resource.binary?
+      resource.respond_to?(:content_type) &&
+      resource.content_type != 'text/x-markdown'
   end
 
-  def to_binary
+  def raw_type
+    if resource.respond_to?(:binary?) && resource.binary?
+      'file'
+    else
+      'data'
+    end
+  end
+
+  def to_file
     controller.send_file resource.body.path, filename: resource.title,
       type: resource.content_type, disposition: :inline
   end
+
+  def to_data
+    controller.send_data resource.body, filename: resource.title,
+      type: resource.content_type, disposition: :inline
+  end
+
 end
