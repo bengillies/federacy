3880907184147ded419e4a506ea753da1148d8c3
Ben Gillies <bengillies@gmail.com>
2014-06-17 13:11:23 +0100

    Accept data uris as file attribute

--- app/models/file_revision.rb
+++ app/models/file_revision.rb
@@ -17,11 +17,45 @@ class FileRevision < ActiveRecord::Base
   end
 
   def set_body! attrs
+    if attrs["file"].class == String
+      # base 64 data uri
+      attrs["file"], attrs["content_type"] = file_from_data_uri(attrs["file"])
+    end
+
     self.file = attrs["file"]
     self.content_type = attrs["content_type"]
   end
 
   protected
+
+  class DataURIIO < StringIO
+    attr_accessor :original_filename, :mime_type
+
+    def self.from_data_uri uri
+      if uri.match(%r{^data:([^;]+)?;([^,]+)?,(.*)$})
+        mime_type = $1
+        encoding = $2
+        data = $3
+        extension = $1.split('/')[1]
+
+        contents = if encoding == 'base64'
+          Base64.decode64(data)
+        else
+          data
+        end
+
+        io = self.new contents
+        io.original_filename = "foo.#{extension}"
+        io.mime_type = mime_type
+        io
+      end
+    end
+  end
+
+  def file_from_data_uri uri
+    decoded_file = DataURIIO.from_data_uri uri
+    [decoded_file, decoded_file.mime_type]
+  end
 
   def set_defaults
     self.content_type = file.content_type unless file.content_type.blank?
