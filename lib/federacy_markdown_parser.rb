b197ae813da9e094d2f5575c64b324356ba57609
Ben Gillies <bengillies@gmail.com>
2014-11-12 12:51:47 +0000

    Use do...end for multiline blocks

--- lib/federacy_markdown_parser.rb
+++ lib/federacy_markdown_parser.rb
@@ -3,22 +3,22 @@ class FederacyMarkdownParser < Parslet::Parser
   ##
   # Types of things to match inside a text block line
   ##
-  rule(:sym) {
+  rule(:sym) do
     inline_code_block |
     tiddlylink |
     markdown_link |
     char
-  }
+  end
 
   ##
   # Types of line
   ##
-  rule(:line) {
+  rule(:line) do
     code_block |
     transclusion |
     footer_reference |
     text_line
-  }
+  end
 
   ##
   # Types of block
@@ -26,12 +26,12 @@ class FederacyMarkdownParser < Parslet::Parser
   # There is more than this in markdown, but we only care about a few as we only
   # need to extract links from them
   ##
-  rule(:block) {
+  rule(:block) do
     code_block |
     transclusion |
     text_block |
     new_line
-  }
+  end
 
   ##
   # TiddlyLinks
@@ -64,76 +64,76 @@ class FederacyMarkdownParser < Parslet::Parser
   rule(:link_user_separator) { str(':') }
   rule(:space_symbol) { str('@') }
 
-  rule(:link_title) {
+  rule(:link_title) do
     (
       link_title_separator.absent? >> eol?.absent? >> any
     ).repeat(1).as(:title) >>
     link_title_separator
-  }
+  end
 
-  rule(:link_body_simple) {
+  rule(:link_body_simple) do
     (link_close.absent? >> eol?.absent? >> any).repeat(1).as(:link)
-  }
-  rule(:link_body_with_title) {
+  end
+  rule(:link_body_with_title) do
     link_title >> link_body_simple
-  }
-  rule(:tiddler_link_body) {
+  end
+  rule(:tiddler_link_body) do
     link_body_with_title |
     link_body_simple
-  }
-  rule(:tiddler_link) {
+  end
+  rule(:tiddler_link) do
     (
       link_open >>
       tiddler_link_body >>
       link_close
     ).as(:tiddler_link)
-  }
+  end
 
-  rule(:link_user_word) {
+  rule(:link_user_word) do
     (
       whitespace.absent? >>
       link_user_separator.absent? >>
       any
     ).repeat(1).as(:user) >>
     link_user_separator
-  }
-  rule(:link_user_body) {
+  end
+  rule(:link_user_body) do
     (link_user_separator.absent? >> eol?.absent? >> any).repeat(1).as(:user) >>
     link_user_separator
-  }
+  end
 
-  rule(:link_word) {
+  rule(:link_word) do
     (whitespace.absent? >> any).repeat(1).as(:link)
-  }
+  end
 
-  rule(:space_link_body) {
+  rule(:space_link_body) do
     link_open >> link_title >> link_user_body >> link_body_simple >> link_close |
     link_open >> link_title >> link_body_simple >> link_close |
     link_open >> link_user_body >> link_body_simple >> link_close |
     link_open >> link_body_simple >> link_close |
     link_user_word >> link_word |
     link_word
-  }
-  rule(:space_link) {
+  end
+  rule(:space_link) do
     (space_symbol >> space_link_body).as(:space_link)
-  }
+  end
 
 
-  rule(:tiddler_link_unbracketed) {
+  rule(:tiddler_link_unbracketed) do
     (
       (space_symbol.absent? >> whitespace.absent? >> any).repeat(1).as(:link)
     ).as(:tiddler_link)
-  }
+  end
 
-  rule(:tiddler_space_link) {
+  rule(:tiddler_space_link) do
     ((tiddler_link | tiddler_link_unbracketed) >> space_link).as(:tiddler_space_link)
-  }
+  end
 
-  rule(:tiddlylink) {
+  rule(:tiddlylink) do
     tiddler_space_link |
     tiddler_link |
     space_link
-  }
+  end
 
   ##
   # Markdown Links
@@ -147,16 +147,16 @@ class FederacyMarkdownParser < Parslet::Parser
   rule(:bracket_close) { str(')') }
   rule(:exclamation_mark) { str('!') }
 
-  rule(:square_body) {
+  rule(:square_body) do
     (square_close.absent? >> image_link.absent? >> any).repeat(1)
-  }
-  rule(:square_link) {
+  end
+  rule(:square_link) do
     square_open >> square_body.as(:title) >> square_close >> str(' ').maybe
-  }
-  rule(:bracket_body_link_only) {
+  end
+  rule(:bracket_body_link_only) do
     (bracket_close.absent? >> any).repeat(1).as(:link)
-  }
-  rule(:bracket_body_with_title_double_quote) {
+  end
+  rule(:bracket_body_with_title_double_quote) do
     (
       (
         bracket_close.absent? >> (
@@ -167,8 +167,8 @@ class FederacyMarkdownParser < Parslet::Parser
       (str('"').absent? >> any).repeat(1).as(:title_attr) >>
       str('"')
     )
-  }
-  rule(:bracket_body_with_title_single_quote) {
+  end
+  rule(:bracket_body_with_title_single_quote) do
     (
       (
         bracket_close.absent? >> (
@@ -179,56 +179,56 @@ class FederacyMarkdownParser < Parslet::Parser
       (str("'").absent? >> any).repeat(1).as(:title_attr) >>
       str("'")
     )
-  }
-  rule(:bracket_body_with_title) {
+  end
+  rule(:bracket_body_with_title) do
     bracket_body_with_title_double_quote | bracket_body_with_title_single_quote
-  }
-  rule(:bracket_body) {
+  end
+  rule(:bracket_body) do
     bracket_body_with_title | bracket_body_link_only
-  }
-  rule(:bracket_section) {
+  end
+  rule(:bracket_section) do
     bracket_open >> bracket_body >> bracket_close
-  }
-  rule(:markdown_base_link) {
+  end
+  rule(:markdown_base_link) do
     square_link >> bracket_section
-  }
-  rule(:standard_reference_base) {
+  end
+  rule(:standard_reference_base) do
     square_open >> square_body.as(:title) >> square_close >>
     match("\s").repeat >>
     square_open >> square_body.as(:reference) >> square_close
-  }
+  end
 
-  rule(:simple_reference_base) {
+  rule(:simple_reference_base) do
     square_open >> square_body.as(:title_and_reference) >> square_close
-  }
-  rule(:standard_image) {
+  end
+  rule(:standard_image) do
     (exclamation_mark >> markdown_base_link).as(:image_link)
-  }
-  rule(:standard_image_with_title) {
+  end
+  rule(:standard_image_with_title) do
     (exclamation_mark >> square_link >> bracket_body_with_title).as(:image_link)
-  }
-  rule(:footer_image) {
+  end
+  rule(:footer_image) do
     exclamation_mark >>
     (standard_reference_base | simple_reference_base).as(:footer_image)
-  }
-  rule(:square_with_image) {
+  end
+  rule(:square_with_image) do
     square_open >> image_link >> square_close >> str(' ').maybe
-  }
+  end
 
-  rule(:standard_link) {
+  rule(:standard_link) do
     (
       (square_with_image >> bracket_section) |
       markdown_base_link
     ).as(:standard_link)
-  }
+  end
 
-  rule(:image_link) {
+  rule(:image_link) do
     standard_image |
     footer_image >> bracket_section.absent? |
     standard_image_with_title
-  }
+  end
 
-  rule(:footer_link) {
+  rule(:footer_link) do
     (
       (
         square_with_image >> match("\s").repeat >>
@@ -238,13 +238,13 @@ class FederacyMarkdownParser < Parslet::Parser
       square_with_image |
       simple_reference_base
     ).as(:footer_link)
-  }
+  end
 
-  rule(:markdown_link) {
+  rule(:markdown_link) do
     image_link |
     standard_link |
     footer_link
-  }
+  end
 
   ##
   # References that footer_links point to
@@ -254,41 +254,41 @@ class FederacyMarkdownParser < Parslet::Parser
   rule(:angle_close) { str('>') }
   rule(:angle_body) { (angle_close.absent? >> eol?.absent? >> any).repeat(1) }
 
-  rule(:footer_reference_start) {
+  rule(:footer_reference_start) do
     square_open >> square_body.as(:reference) >> square_close >>
     footer_separator
-  }
+  end
 
-  rule(:footer_reference_title_double_quote) {
+  rule(:footer_reference_title_double_quote) do
     whitespace.repeat >>
     str('"') >> (str('"').absent? >> any).repeat(1).as(:title_attr) >>
     str('"')
-  }
+  end
 
-  rule(:footer_reference_title_single_quote) {
+  rule(:footer_reference_title_single_quote) do
     whitespace.repeat >>
     str("'") >> (str("'").absent? >> any).repeat(1).as(:title_attr) >>
     str("'")
-  }
+  end
 
-  rule(:footer_reference_title_bracket) {
+  rule(:footer_reference_title_bracket) do
     whitespace.repeat >>
     str('(') >> (str(')').absent? >> any).repeat(1).as(:title_attr) >>
     str(')')
-  }
+  end
 
-  rule(:footer_reference_title) {
+  rule(:footer_reference_title) do
     footer_reference_title_double_quote |
     footer_reference_title_single_quote |
     footer_reference_title_bracket
-  }
+  end
 
-  rule(:footer_reference_with_angles) {
+  rule(:footer_reference_with_angles) do
     footer_reference_start >>
     angle_open >> angle_body.as(:link) >> angle_close >>
     footer_reference_title.maybe
-  }
-  rule(:footer_reference_without_angles_with_title_double_quote) {
+  end
+  rule(:footer_reference_without_angles_with_title_double_quote) do
     footer_reference_start >>
     (
       (
@@ -296,8 +296,8 @@ class FederacyMarkdownParser < Parslet::Parser
       ).absent? >> eol?.absent? >> any
     ).repeat(1).as(:link) >>
     footer_reference_title_double_quote
-  }
-  rule(:footer_reference_without_angles_with_title_single_quote) {
+  end
+  rule(:footer_reference_without_angles_with_title_single_quote) do
     footer_reference_start >>
     (
       (
@@ -305,8 +305,8 @@ class FederacyMarkdownParser < Parslet::Parser
       ).absent? >> eol?.absent? >> any
     ).repeat(1).as(:link) >>
     footer_reference_title_single_quote
-  }
-  rule(:footer_reference_without_angles_with_title_bracket) {
+  end
+  rule(:footer_reference_without_angles_with_title_bracket) do
     footer_reference_start >>
     (
       (
@@ -314,24 +314,24 @@ class FederacyMarkdownParser < Parslet::Parser
       ).absent? >> eol?.absent? >> any
     ).repeat(1).as(:link) >>
     footer_reference_title_bracket
-  }
-  rule(:footer_reference_without_angles_with_title) {
+  end
+  rule(:footer_reference_without_angles_with_title) do
     footer_reference_without_angles_with_title_double_quote |
     footer_reference_without_angles_with_title_single_quote |
     footer_reference_without_angles_with_title_bracket
-  }
+  end
 
-  rule(:footer_reference_without_angles) {
+  rule(:footer_reference_without_angles) do
     footer_reference_start >> (eol?.absent? >> any).repeat(1).as(:link)
-  }
+  end
 
-  rule(:footer_reference) {
+  rule(:footer_reference) do
     (
       footer_reference_with_angles |
       footer_reference_without_angles_with_title |
       footer_reference_without_angles
     ).as(:footer_reference) >> eol?
-  }
+  end
 
   ##
   # Transclusions
@@ -345,7 +345,7 @@ class FederacyMarkdownParser < Parslet::Parser
   # {{{[[Abraham Lincoln]]@jon-wilkes-booth:people-to-kill}}}
   ##
 
-  rule(:space_link_insude_transclusion) {
+  rule(:space_link_insude_transclusion) do
     (
       space_symbol >>
       (
@@ -365,30 +365,30 @@ class FederacyMarkdownParser < Parslet::Parser
         any
       ).repeat(1).as(:link)
     ).as(:space_link)
-  }
-  rule(:tiddlylink_inside_transclusion) {
+  end
+  rule(:tiddlylink_inside_transclusion) do
     # handle tiddlylinks where the user/space name isn't enclosed in square brackets
     (
       (tiddler_link | tiddler_link_unbracketed) >>
       space_link_insude_transclusion
     ).as(:tiddler_space_link) |
     space_link_insude_transclusion.as(:space_link)
-  }
+  end
 
   rule(:transclusion_start) { str('{{{') }
   rule(:transclusion_end) { str('}}}') }
-  rule(:transclusion_tiddler) {
-    tiddlylink_inside_transclusion | # TODO: implement tiddlylink_inside_transclusion
+  rule(:transclusion_tiddler) do
+    tiddlylink_inside_transclusion |
     tiddlylink |
     (transclusion_end.absent? >> eol?.absent? >> any).repeat(1).as(:link)
-  }
-  rule(:transclusion) {
+  end
+  rule(:transclusion) do
     (
       transclusion_start >>
       transclusion_tiddler >>
       transclusion_end >> eol?
     ).as(:transclusion)
-  }
+  end
 
   ##
   # Text block definition
@@ -413,31 +413,31 @@ class FederacyMarkdownParser < Parslet::Parser
   rule(:same_line?) { (new_line.absent? >> any).repeat(0) }
 
   rule(:inline_code?) { (backtick.absent? >> any).repeat(1) }
-  rule(:inline_code_block_unquoted) {
+  rule(:inline_code_block_unquoted) do
     backtick >> inline_code? >> backtick
-  }
-  rule(:inline_code_block_quoted) {
+  end
+  rule(:inline_code_block_quoted) do
     backtick.repeat(1).capture(:backticks) >>
     dynamic do |source, context|
       (str(context.captures[:backticks]).absent? >> any).repeat(0) >>
       str(context.captures[:backticks])
     end
-  }
-  rule(:inline_code_block) {
+  end
+  rule(:inline_code_block) do
     (inline_code_block_unquoted | inline_code_block_quoted).as(:inline_code)
-  }
+  end
 
-  rule(:code_block_backtick) {
+  rule(:code_block_backtick) do
     backticks >> same_line? >> new_line >>
     ((new_line >> backticks >> eol?).absent? >> any).repeat >>
     (new_line >> backticks >> eol? | eof?)
-  }
-  rule(:code_block_tilde) {
+  end
+  rule(:code_block_tilde) do
       tildes >> same_line? >> new_line >>
       ((new_line >> tildes >> eol?).absent? >> any).repeat >>
       (new_line >> tildes >> eol? | eof?)
-  }
-  rule(:code_block_whitespace_sof) {
+  end
+  rule(:code_block_whitespace_sof) do
     dynamic do |source, context|
       if source.pos.charpos == 0
         match('.').present?
@@ -446,21 +446,21 @@ class FederacyMarkdownParser < Parslet::Parser
       end
     end >>
     (code_tab >> same_line? >> new_line.maybe).repeat(1)
-  }
-  rule(:code_block_whitespace_newline) {
+  end
+  rule(:code_block_whitespace_newline) do
     match("\s").repeat >> new_line >>
     (code_tab >> same_line? >> new_line.maybe).repeat(1)
-  }
-  rule(:code_block_whitespace) {
+  end
+  rule(:code_block_whitespace) do
     code_block_whitespace_newline | code_block_whitespace_sof
-  }
-  rule(:code_block) {
+  end
+  rule(:code_block) do
     (
       code_block_backtick |
       code_block_tilde |
       code_block_whitespace
     ).as(:code_block)
-  }
+  end
 
 
   ##
@@ -474,9 +474,9 @@ class FederacyMarkdownParser < Parslet::Parser
   ##
   # Entry point of parser
   ##
-  rule(:document) {
+  rule(:document) do
     block.repeat
-  }
+  end
   root(:document)
 
 
