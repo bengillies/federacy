f38183b66f7020062feafa8dbdb4552332d6fa35
Ben Gillies <bengillies@gmail.com>
2014-11-20 14:42:29 +0000

    Improve the link serializer

--- app/serializers/revision_link_serializer.rb
+++ app/serializers/revision_link_serializer.rb
@@ -1,4 +1,20 @@
 class RevisionLinkSerializer < ApplicationSerializer
   attributes :start, :end, :link_type, :link, :tiddler_title, :space_name,
-    :user_name,  :from, :to
+    :user_name, :from, :to
+
+  def from
+    {
+      tiddler_id:  object.from.id,
+      space_id:    object.from.space.id,
+      revision_id: object.revision_id
+    }
+  end
+
+  def to
+    {
+      tiddler_id:  object.tiddler_id,
+      space_id:    object.space_id,
+      revision_id: object.target_id
+    }
+  end
 end
